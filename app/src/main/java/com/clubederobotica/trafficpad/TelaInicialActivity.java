a9376347577d8c99a645ee5703d74a317879a031
Michel Ribeiro <michel.ribeiro1@hotmail.com>
2017-07-25 01:19:16 -0300

    Finalizadas as duas primeiras Activities: Tela Inicial e Tela Funcional

---
+++ app/src/main/java/com/clubederobotica/trafficpad/TelaInicialActivity.java
@@ -0,0 +1,77 @@
+package com.clubederobotica.trafficpad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Aperte no ícone para iniciar a aplicação.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btn_abrir_app = (Button) findViewById(R.id.botao_iniciar_app);
+
+        btn_abrir_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TelaInicialActivity.this, TelaFuncionalActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tela_inicial, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(id){
+            case R.id.action_close:
+                finish();
+                break;
+            case R.id.action_sobre:
+                Toast.makeText(this, "@TrafficPad, versão 2.0. Disponha!", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        if (id == R.id.action_sobre) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
